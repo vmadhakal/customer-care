6738e1ce9583e12d5f84d96286efb2d6637e5f20
vmadhakal <53706630+vmadhakal@users.noreply.github.com>
2019-11-06 22:06:41 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("This will be printed");
-	System.out.println("This is the first java program");
+	
 	    System.out.println("This will trigger another build");
 	
     }
